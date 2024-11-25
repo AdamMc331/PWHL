4e4f8ab6d92b3326292756b153dfa3eab602b7cc
Adam McNeilly <amcneilly331@gmail.com>
2024-11-25 17:37:28 -0500

    Screen setup works.

--- shared/src/commonMain/kotlin/com/pwhl/mobile/shared/feed/FeedScreen.kt
+++ shared/src/commonMain/kotlin/com/pwhl/mobile/shared/feed/FeedScreen.kt
@@ -1,7 +1,12 @@
 package com.pwhl.mobile.shared.feed
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import com.pwhl.mobile.shared.ui.components.GameListItem
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -10,4 +15,13 @@ fun FeedScreen(
 ) {
     val viewModel: FeedViewModel = koinViewModel()
     viewModel.fetchData()
+    val state = viewModel.state.collectAsState()
+
+    LazyColumn {
+        items(state.value.recentGames) { game ->
+            GameListItem(game)
+
+            HorizontalDivider()
+        }
+    }
 }
--- shared/src/commonMain/kotlin/com/pwhl/mobile/shared/feed/FeedViewModel.kt
+++ shared/src/commonMain/kotlin/com/pwhl/mobile/shared/feed/FeedViewModel.kt
@@ -2,16 +2,35 @@ package com.pwhl.mobile.shared.feed
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.pwhl.mobile.shared.displaymodels.GameDisplayModel
 import com.pwhl.mobile.shared.domain.usecases.FetchRecentGamesUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class FeedViewModel(
     private val recentGamesUseCase: FetchRecentGamesUseCase,
 ) : ViewModel() {
+    private val mutableState = MutableStateFlow(
+        FeedState(
+            recentGames = emptyList(),
+        ),
+    )
+    val state = mutableState.asStateFlow()
+
     fun fetchData() {
         viewModelScope.launch {
             val x = recentGamesUseCase.invoke()
             println("ADAMLOG - $x")
+            val gameList = x.getOrNull()?.map { game ->
+                GameDisplayModel(game)
+            }
+            mutableState.update { currentState ->
+                currentState.copy(
+                    recentGames = gameList.orEmpty(),
+                )
+            }
         }
     }
 }
