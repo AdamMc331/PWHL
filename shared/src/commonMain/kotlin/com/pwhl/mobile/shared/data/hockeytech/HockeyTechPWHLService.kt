48d9f9b8c7b526e53fa6f24e4639aba8f7ecfa62
Adam McNeilly <amcneilly331@gmail.com>
2024-12-07 15:11:59 -0500

    Adding object files for common param keys and values.

--- shared/src/commonMain/kotlin/com/pwhl/mobile/shared/data/hockeytech/HockeyTechPWHLService.kt
+++ shared/src/commonMain/kotlin/com/pwhl/mobile/shared/data/hockeytech/HockeyTechPWHLService.kt
@@ -1,6 +1,7 @@
 package com.pwhl.mobile.shared.data.hockeytech
 
 import com.pwhl.mobile.shared.BuildKonfig
+import com.pwhl.mobile.shared.data.hockeytech.dto.HockeyTechParameterKeys
 import com.pwhl.mobile.shared.data.hockeytech.dto.HockeyTechScoreBarResponseDTO
 import com.pwhl.mobile.shared.data.hockeytech.dto.HockeyTechStandingsListResponseDTO
 import com.pwhl.mobile.shared.data.remote.BaseKtorClient
@@ -26,11 +27,11 @@ class HockeyTechPWHLService(
         val daysAhead = now.daysUntil(request.beforeDate, TimeZone.currentSystemDefault())
 
         val gameListParams = mapOf(
-            PARAM_KEY_FEED to PARAM_VALUE_MODULE_KIT,
-            PARAM_KEY_VIEW to PARAM_VALUE_SCORE_BAR,
-            PARAM_KEY_DAYS_AHEAD to daysAhead.toString(),
-            PARAM_KEY_DAYS_BACK to daysBack.toString(),
-            PARAM_KEY_TEAM_ID to request.teamId.orEmpty(),
+            HockeyTechParameterKeys.FEED to "modulekit",
+            HockeyTechParameterKeys.VIEW to "scorebar",
+            HockeyTechParameterKeys.DAYS_AHEAD to daysAhead.toString(),
+            HockeyTechParameterKeys.DAYS_BACK to daysBack.toString(),
+            HockeyTechParameterKeys.TEAM_ID to request.teamId.orEmpty(),
         )
 
         val params = HockeyTechKtorClient.baseHockeyTechParams + gameListParams
@@ -69,16 +70,5 @@ class HockeyTechPWHLService(
                 data?.parseStandingsRow()
             }.orEmpty()
         }
-    }
-
-    companion object {
-        private const val PARAM_KEY_FEED = "feed"
-        private const val PARAM_KEY_VIEW = "view"
-        private const val PARAM_KEY_DAYS_AHEAD = "numberofdaysahead"
-        private const val PARAM_KEY_DAYS_BACK = "numberofdaysback"
-        private const val PARAM_KEY_TEAM_ID = "team_id"
-
-        private const val PARAM_VALUE_MODULE_KIT = "modulekit"
-        private const val PARAM_VALUE_SCORE_BAR = "scorebar"
     }
 }
