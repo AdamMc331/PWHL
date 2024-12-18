532d9081bd282102cdc68d955fe2fe7af151417f
Adam McNeilly <amcneilly331@gmail.com>
2024-12-17 19:15:54 -0500

    Adding faceoff details.

---
+++ shared/src/commonMain/kotlin/com/adammcneilly/pwhl/mobile/shared/data/hockeytech/dto/HockeyTechFaceOffDetailsDTO.kt
@@ -0,0 +1,22 @@
+package com.adammcneilly.pwhl.mobile.shared.data.hockeytech.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class HockeyTechFaceOffDetailsDTO(
+    @SerialName("homePlayer")
+    val homePlayer: HockeyTechPlayerSummaryDTO? = null,
+    @SerialName("homeWin")
+    val homeWin: String? = null,
+    @SerialName("period")
+    val period: HockeyTechPeriodDTO? = null,
+    @SerialName("time")
+    val time: String? = null,
+    @SerialName("visitingPlayer")
+    val visitingPlayer: HockeyTechPlayerSummaryDTO? = null,
+    @SerialName("xLocation")
+    val xLocation: Int? = null,
+    @SerialName("yLocation")
+    val yLocation: Int? = null,
+)
