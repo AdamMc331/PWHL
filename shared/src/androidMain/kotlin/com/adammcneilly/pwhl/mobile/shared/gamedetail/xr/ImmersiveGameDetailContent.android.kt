b2b2b4af2c46155b70f41721c2c79ffb4be15eba
Adam McNeilly <amcneilly331@gmail.com>
2024-12-23 13:10:04 -0500

    Tweaking padding and sizes.

--- shared/src/androidMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/xr/ImmersiveGameDetailContent.android.kt
+++ shared/src/androidMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/xr/ImmersiveGameDetailContent.android.kt
@@ -1,6 +1,5 @@
 package com.adammcneilly.pwhl.mobile.shared.gamedetail.xr
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.xr.compose.spatial.Subspace
@@ -8,41 +7,71 @@ import androidx.xr.compose.subspace.SpatialColumn
 import androidx.xr.compose.subspace.SpatialRow
 import androidx.xr.compose.subspace.layout.SubspaceModifier
 import androidx.xr.compose.subspace.layout.fillMaxWidth
+import androidx.xr.compose.subspace.layout.height
+import androidx.xr.compose.subspace.layout.padding
 import androidx.xr.compose.subspace.layout.width
 import com.adammcneilly.pwhl.mobile.shared.gamedetail.GameDetailState
+import com.adammcneilly.pwhl.mobile.shared.gamedetail.GameDetailStatsTab
+import com.adammcneilly.pwhl.mobile.shared.gamedetail.GameDetailSummaryTab
+import com.adammcneilly.pwhl.mobile.shared.gamedetail.playbyplay.PlayByPlayList
 import com.adammcneilly.pwhl.mobile.shared.ui.components.SpatialSurface
 
 @Composable
 actual fun ImmersiveGameDetailContent(
     state: GameDetailState,
 ) {
+    if (state.game == null) {
+        // Need to handle loading/error state.
+        // Ideally we don't even allow entering immersive UI without game.
+        return
+    }
+
     Subspace {
         SpatialColumn(
             modifier = SubspaceModifier
                 .width(1200.dp), // Determine where this number came from and how to set a practical default
         ) {
-            if (state.game != null) {
+            SpatialSurface(
+                modifier = SubspaceModifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+            ) {
+                SpatialGameDetailHeader(
+                    game = state.game,
+                )
+            }
+
+            SpatialRow(
+                modifier = SubspaceModifier
+                    .width(1200.dp), // Determine where this number came from and how to set a practical default
+            ) {
                 SpatialSurface(
                     modifier = SubspaceModifier
-                        .fillMaxWidth(),
+                        .height(600.dp)
+                        .weight(1F),
                 ) {
-                    SpatialGameDetailHeader(
+                    GameDetailSummaryTab(
                         game = state.game,
                     )
                 }
-            }
 
-            SpatialRow {
-                SpatialSurface {
-                    Text("MVP Content")
+                SpatialSurface(
+                    modifier = SubspaceModifier
+                        .height(600.dp)
+                        .weight(1F)
+                        .padding(horizontal = 16.dp),
+                ) {
+                    GameDetailStatsTab()
                 }
 
-                SpatialSurface {
-                    Text("Stat Content")
-                }
-
-                SpatialSurface {
-                    Text("PBP Content")
+                SpatialSurface(
+                    modifier = SubspaceModifier
+                        .height(600.dp)
+                        .weight(1F),
+                ) {
+                    PlayByPlayList(
+                        events = state.playByPlayEvents,
+                    )
                 }
             }
         }
