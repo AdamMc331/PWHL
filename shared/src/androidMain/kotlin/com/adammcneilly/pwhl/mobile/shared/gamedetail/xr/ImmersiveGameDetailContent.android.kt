982dbaa494718c637f1ad0fa2bce6dcb09274a05
Adam McNeilly <amcneilly331@gmail.com>
2024-12-23 13:28:28 -0500

    Tweaking padding and sizes.

--- shared/src/androidMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/xr/ImmersiveGameDetailContent.android.kt
+++ shared/src/androidMain/kotlin/com/adammcneilly/pwhl/mobile/shared/gamedetail/xr/ImmersiveGameDetailContent.android.kt
@@ -1,7 +1,23 @@
 package com.adammcneilly.pwhl.mobile.shared.gamedetail.xr
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CloseFullscreen
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.xr.compose.spatial.EdgeOffset.Companion.inner
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterEdge
 import androidx.xr.compose.spatial.Subspace
 import androidx.xr.compose.subspace.SpatialColumn
 import androidx.xr.compose.subspace.SpatialRow
@@ -15,11 +31,17 @@ import com.adammcneilly.pwhl.mobile.shared.gamedetail.GameDetailStatsTab
 import com.adammcneilly.pwhl.mobile.shared.gamedetail.GameDetailSummaryTab
 import com.adammcneilly.pwhl.mobile.shared.gamedetail.playbyplay.PlayByPlayList
 import com.adammcneilly.pwhl.mobile.shared.ui.components.SpatialSurface
+import com.adammcneilly.pwhl.mobile.shared.xr.LocalXRSession
 
 @Composable
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 actual fun ImmersiveGameDetailContent(
     state: GameDetailState,
 ) {
+    // Session should not be null to even get here, so
+    // maybe we modify the funciton to consume a non nullable session.
+    val session = LocalXRSession.current ?: return
+
     if (state.game == null) {
         // Need to handle loading/error state.
         // Ideally we don't even allow entering immersive UI without game.
@@ -36,6 +58,35 @@ actual fun ImmersiveGameDetailContent(
                     .fillMaxWidth()
                     .padding(bottom = 16.dp),
             ) {
+                Orbiter(
+                    position = OrbiterEdge.Top,
+                    offset = inner(8.dp),
+                    alignment = Alignment.End,
+                ) {
+                    Surface(
+                        shape = CircleShape,
+                        modifier = Modifier
+                            .padding(end = 16.dp),
+                    ) {
+                        IconButton(
+                            onClick = {
+                                session.requestHomeSpaceMode()
+                            },
+                            colors = IconButtonDefaults.iconButtonColors(
+                                containerColor = MaterialTheme.colorScheme.surface,
+                                contentColor = MaterialTheme.colorScheme.onSurface,
+                            ),
+                            modifier = Modifier
+                                .size(IconButtonDefaults.mediumContainerSize()),
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.CloseFullscreen,
+                                contentDescription = "Exit Full Space Mode",
+                            )
+                        }
+                    }
+                }
+
                 SpatialGameDetailHeader(
                     game = state.game,
                 )
